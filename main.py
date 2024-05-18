e497fc26acd32d8ba8beb502001071ccda6787aa
Nikolaos Takos <takosnikolaos@gmail.com>
2024-05-18 17:59:08 +0300

    Finished login function
    Finished login function

--- main.py
+++ main.py
@@ -20,4 +20,29 @@ mysql = MySQL(app)
 # http://localhost:5000/pythonlogin/ - this is the login page, we need to use both GET and POST requests
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
-    return render_template('index.html', msg='')
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+        # Show the login form with message (if any)
+        return render_template('index.html', msg=msg)
+
